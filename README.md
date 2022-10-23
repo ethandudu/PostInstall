f9635445a20c553b910e3f2788f3a552c9e14574
Baptiste Paqueriaud <paqueriaudbaba@gmail.com>
2022-10-23 23:31:17 +0200

    Passage aux whiptail

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # PostInstall
-A post installation script for Linux
+Un script post installation pour Linux.  
 
-Only for Linux distros using APT like Debian, Ubuntu, Raspbian etc.
+<br/>
+
+A noter que ce script ne fonctionne que pour les distributions utilisant le gestionnaire de paquet `apt`, celles-ci sont en grande partie les distributions basées sur `Debian`, `Ubunutu`. Ce script est donc compatible avec `Raspbian`
+
+<br/>
+
+Pour lancer le script :
+```bash
+$ chmod u+x post_install_APT.sh
+```
+Donnera la permission au script de s'exécuter.  
+
+```bash
+$ ./post_install_APT.sh
+```
+Exécutera le script
--- post_install_APT.sh
+++ post_install_APT.sh
@@ -1,60 +1,63 @@
-#!/bin/sh
+#!/bin/bash
 
-# Lancement
-echo -e "\e[39m"
-echo -e "\e[34m============================="
-echo -e "\e[39m"
-echo -e "\e[36mBienvenue sur votre machine !"
-echo -e "\e[39m"
-echo -e "\e[34m============================="
-echo -e "\e[39m"
-echo -e "\e[39m"
+if [ "$EUID" != 0 ] #Demande les accès administrateur si l'utilisateur n'est pas super user
+then
+    sudo "$0" "$@"
+    exit $?
+fi
 
-# Update et upgrade APT
-echo -e "\e[34m=================================================================="
-echo -e "\e[39m"
-echo -e "\e[33m Mise à jour de la liste des paquets et mise à niveau en cours ..."
-echo -e "\e[39m"
-echo -e "\e[34m=================================================================="
-echo -e "\e[90"
 sudo apt update -y && sudo apt upgrade -y
-echo -e "\e[39m"
-echo -e "\e[32mMise à jour effectuée"
-echo -e "\e[39m"
-echo -e "\e[39m"
 
-# Installation et affichage de Neofetch
-echo -e "\e[34m================================="
-echo -e "\e[39m"
-echo -e "\e[33mInstallation de neofetch en cours \e[39m"
-echo -e "\e[39m"
-echo -e "\e[34m================================="
-echo -e "\e[39m"
-sudo apt install neofetch
-cd ~/
-echo "neofetch" >> .bashrc
-echo -e "\e[39m"
-echo -e "\e[32mInstallation de neofetch terminée"
-echo -e "\e[39m"
-echo -e "\e[36mVotre machine : \e[39m"
-neofetch
-echo -e "\e[39m"
-echo -e "\e[39m"
 
-# Installation des paquets auxiliaires
-echo -e "\e[34m===================================="
-echo -e "\e[39m"
-echo -e "\e[33mInstallation des paquets auxiliaires \e[39m"
-echo -e "\e[39m"
-echo -e "\e[34m===================================="
-echo -e "\e[39m"
-sudo apt install php apache2 python3 curl wget net-tools mariadb-client mariadb-server phpmyadmin php-curl php-mbstring git gcc g++ build-essential vim htop
-echo -e "\e[39m"
-echo -e "\e[39m"
+if whiptail --title "PostInstall" --yesno "Bienvenue dans le script d'installation PostInstall, ce script installera les paquets nécessaires à l'utilsation de votre distribution linux." 8 78 --yes-button "Continuer" --no-button "Quitter";
+then
 
-# Fin du script
-echo -e "\e[34m====================="
-echo -e "\e[39m"
-echo -e "\e[32mInstallation terminée"
-echo -e "\e[39m"
-echo -e "\e[34m====================="
+    SELECTED=$(whiptail --title "Sélection des paquets à installer" --checklist "Sélectionnez les paquest à installer (il est conseillé de tous les installer)" 20 75 10 \
+    php "Pour de développement en php" ON \
+    php-curl "Complément pour php" ON \
+    php-mbstring "Complément pour php" ON \
+    apache2 "Pour l'hébergement web" ON \
+    mariadb-client "Pour la création de base données" ON \
+    mariadb-server "Pour la création de base données" ON \
+    phpmyadmin "Pour la gestion de base données" ON \
+    python3 "Pour le développement en python" ON \
+    curl "Utilisé pour le téléchargement à disance" ON \
+    wget "Utilisé pour le téléchargement de fichiers" ON \
+    net-tools "Set d'utilitaires réseau" ON \
+    git "Utilisé pour le contrôle de version" ON \
+    gcc "Compilateur C" ON \
+    g++ "Compilateur C++" ON \
+    make "Constructeur de fichiers" ON \
+    build-essentials "Essentiels pour la compilation" ON \
+    vim "Meilleur éditeur de text" ON \
+    htop "top, en mieux" ON 3>&1 1>&2 2>&3) #Redirection de la sortie
+
+
+    SELECTED=$(echo $SELECTED | sed 's/"//g') #Retire les "" par substitution
+
+    for i in $SELECTED #Pour chaque paquet sélectionné ...
+    do
+        sudo apt install -y $i
+    done
+
+    whiptail --title "Terminé" --msgbox "L'installation des paquets sélectionnés est terminée." 10 50
+
+    if whiptail --title "Paquets supplémentaires" --yesno "Souhaitez vous installer des paquets supplémentaires (non essentiels) ?" 8 78 --yes-button "Oui" --no-button "Non, bloat is bloat";
+    then
+
+        SELECTED_OPT=$(whiptail --title "Paquets supplémentaire" --checklist "Les paquets ci-dessous sont nons essentiels et de ce fait ne doivent être installé que si vous avez \"l'utilité\"" 20 65 10 \
+        neofetch "Pour montrer à tout le monde votre distro" OFF \
+        cowsay "Fait parler une vache, pourquoi pas ?" OFF \
+        figlet "Fait de jolis textes en ASCII Art" OFF  3>&1 1>&2 2>&3) #Redirection de la sortie
+
+        SELECTED_OPT=$(echo $SELECTED_OPT | sed 's/"//g') #Retire les "" par substitution
+        for i in $SELECTED_OPT
+        do
+            sudo apt install -y $i
+        done
+
+    fi
+
+    whiptail --title "Terminé" --msgbox "L'installation est terminée, merci d'avoir utilisé PostInstall" 10 50
+
+fi
